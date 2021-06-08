121da91b51f1b193c42dc4b01eca579c2a2c35b1
CentOS <centos@centos7.linuxvmimages.local>
2021-06-07 23:17:24 -0400

    Initial PR Commit

---
+++ src/main/java/demo/selenium/com/App.java
@@ -0,0 +1,30 @@
+package demo.selenium.com;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.*;
+import org.testng.annotations.Test;
+public class App
+{
+ @Test
+
+ public static void main(String[] args) {
+
+
+ System.setProperty("webdriver.gecko.driver","/home/centos/geckodriver");
+ FirefoxOptions options = new FirefoxOptions();
+ WebDriver driver = new FirefoxDriver(options);
+
+ System.out.println("Hi, Welcome to Edureka's YouTube Live session on Selenium WebDriver");
+
+
+ driver.get("http://localhost:3001");
+ driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+ //driver.findElement(By.className("v-button")).click();
+ driver.findElement(By.name("login")).click();
+ driver.findElement(By.name("login")).sendKeys("su");
+ driver.findElement(By.name("password")).sendKeys("gw");
+ driver.findElement(By.name("click")).click();
+ }
+}
